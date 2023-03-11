80519340f7845f9e146a55db874ab30acaf1f600
Karamanova <111389457+Karamanova@users.noreply.github.com>
2023-03-11 20:58:23 +0000

    Deploying to gh-pages from @ Karamanova/goit-react-hw-05-movies@ff574ef4f4f1360574b9ab0f52221743c8dfe7e9 🚀

---
+++ static/js/856.8fa34471.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[856],{6856:function(t,a,e){e.d(a,{U1p:function(){return i}});var n=e(9983);function i(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"}},{tag:"path",attr:{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"}}]})(t)}}}]);
+//# sourceMappingURL=856.8fa34471.chunk.js.map
